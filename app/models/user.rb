61649543a930784c18463bf391bbcb155f6b24b6
elCorsaiR <alezander82@mail.ru>
2015-10-30 20:01:21 +0200

    process parsing if file present

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
 
   attr_accessor :file
 
-  validates :file, presence: true, on: :create, unless:  Proc.new { |user| user.admin? }
+  validates :file, presence: true, on: :create, unless: Proc.new { |user| user.admin? }
   validates :email, presence: true, uniqueness: {case_sensitive: false}
   validates :password, length: {minimum: 6}, :if => :should_validate_password?
 
@@ -72,35 +72,37 @@ class User < ActiveRecord::Base
   end
 
   def parse_file
-    spreadsheet = User::open_spreadsheet(file)
-    puts "============== Last row:#{spreadsheet.last_row}"
+    if file.present?
+      spreadsheet = User::open_spreadsheet(file)
+      puts "============== Last row:#{spreadsheet.last_row}"
 
-    (2..spreadsheet.last_row-1).each do |i|
-      row = spreadsheet.row(i)
-
-
-      self.report_date = row[1] if row[0] == 'FECHA'
-      first_name = row[1] if row[0] == 'NOMBRE'
-      last_name = row[1] if row[0] == 'APELLIDOS'
-      self.birthday = row[1] if row[0] == 'EDAD'
-
-      # self.birthday = row[1] if row[0] == 'SEXO'
-      self.weight = row[1] if row[0] == 'PESO'
-      self.height = row[1] if row[0] == 'ALTURA'
-      self.level_id = Level.find_or_create_by(name: row[1]).id if row[0] == 'NIVEL'
-      self.sport_id = Sport.find_or_create_by(name: row[1]).id if row[0] == 'DEPORTE'
+      (2..spreadsheet.last_row-1).each do |i|
+        row = spreadsheet.row(i)
 
 
-      self.injury = row[1] if row[0] == 'LESIÓN'
+        self.report_date = row[1] if row[0] == 'FECHA'
+        first_name = row[1] if row[0] == 'NOMBRE'
+        last_name = row[1] if row[0] == 'APELLIDOS'
+        self.birthday = row[1] if row[0] == 'EDAD'
 
-      self.ankle = (row[1]*100).to_i if row[0] == 'PERFORMANCE INDEX TOBILLO'
-      self.knee = (row[1]*100).to_i if row[0] == 'PERFORMANCE INDEX RODILLA'
-      self.hip = (row[1]*100).to_i if row[0] == 'PERFORMANCE INDEX CADERA'
-      self.functionality = (row[1]*100).to_i if row[0] == 'PERFORMANCE INDEX FUNCIONALIDAD'
-      self.total = (row[1]*100).to_i if row[0] == 'PERFORMANCE INDEX TOTAL'
+        # self.birthday = row[1] if row[0] == 'SEXO'
+        self.weight = row[1] if row[0] == 'PESO'
+        self.height = row[1] if row[0] == 'ALTURA'
+        self.level_id = Level.find_or_create_by(name: row[1]).id if row[0] == 'NIVEL'
+        self.sport_id = Sport.find_or_create_by(name: row[1]).id if row[0] == 'DEPORTE'
+
+
+        self.injury = row[1] if row[0] == 'LESIÓN'
+
+        self.ankle = (row[1]*100).to_i if row[0] == 'PERFORMANCE INDEX TOBILLO'
+        self.knee = (row[1]*100).to_i if row[0] == 'PERFORMANCE INDEX RODILLA'
+        self.hip = (row[1]*100).to_i if row[0] == 'PERFORMANCE INDEX CADERA'
+        self.functionality = (row[1]*100).to_i if row[0] == 'PERFORMANCE INDEX FUNCIONALIDAD'
+        self.total = (row[1]*100).to_i if row[0] == 'PERFORMANCE INDEX TOTAL'
+      end
+
+      # self.name = "#{first_name} #{last_name}".squish
     end
-
-    # self.name = "#{first_name} #{last_name}".squish
 
   end
 
